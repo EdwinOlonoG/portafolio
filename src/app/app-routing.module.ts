4c6ce3410bafd368fa72042aa1f241f9f13b51be
edwin <edwin.olono@gmail.com>
2024-10-19 21:09:27 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingComponent },
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: 'habilidades', component: HabilidadesComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ProyectosComponent,
+    HabilidadesComponent,
+    ContactoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
